0aa3d0f31b1b7974c9563df3b3d55f987677c8f5
Ankita Saha <manashiankitaashok@gmail.com>
2024-07-30 11:20:49 +0530

    Add files via upload

---
+++ flaskapp.py
@@ -0,0 +1,164 @@
+import io
+
+from flask import Flask, render_template, Response,jsonify,request,session,redirect,send_file,url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+#FlaskForm--> it is required to receive input from the user
+# Whether uploading a video file  to our object detection model
+
+from flask_wtf import FlaskForm
+from ultralytics import YOLO
+
+from wtforms import FileField, SubmitField,StringField,DecimalRangeField,IntegerRangeField
+from werkzeug.utils import secure_filename, send_from_directory
+from wtforms.validators import InputRequired,NumberRange
+import os
+
+
+# Required to run the YOLOv8 model
+import cv2
+
+# YOLO_Video is the python file which contains the code for our object detection model
+#Video Detection is the Function which performs Object Detection on Input Video
+from YOLO_Video import video_detection
+from YOLO_image import image_detection
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'bipronil'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'user-system'
+
+mysql = MySQL(app)
+
+#Use FlaskForm to get input video file  from user
+class UploadFileForm(FlaskForm):
+    #We store the uploaded video file path in the FileField in the variable file
+    #We have added validators to make sure the user inputs the video in the valid format  and user does upload the
+    #video when prompted to do so
+    file = FileField("File",validators=[InputRequired()])
+    submit = SubmitField("Run")
+
+
+def generate_frames(path_x = ''):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+
+def generate_frames_web(path_x):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/home', methods=['GET','POST'])
+def home():
+    session.clear()
+    return render_template('indexproject.html')
+# Rendering the Webcam Rage
+#Now lets make a Webcam page for the application
+#Use 'app.route()' method, to render the Webcam page at "/webcam"
+@app.route("/webcam", methods=['GET','POST'])
+
+def webcam():
+    session.clear()
+    return render_template('ui.html')
+
+@app.route('/FrontPage', methods=['GET','POST'])
+def front():
+    # Upload File Form: Create an instance for the Upload File Form
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        # Our uploaded video file path is saved here
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                               secure_filename(file.filename)))  # Then save the file
+        # Use session storage to save video file path
+        session['video_path'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                                             secure_filename(file.filename))
+    return render_template('videoprojectnew.html', form=form)
+@app.route('/video')
+def video():
+    #return Response(generate_frames(path_x='static/files/bikes.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(path_x = session.get('video_path', None)),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+@app.route('/webapp')
+def webapp():
+    #return Response(generate_frames(path_x = session.get('video_path', None),conf_=round(float(session.get('conf_', None))/100,2)),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    mesage = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password,))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            mesage = 'Logged in successfully !'
+            return render_template('user.html', mesage=mesage)
+        else:
+            mesage = 'Please enter correct email / password !'
+    return render_template('login.html', mesage=mesage)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    mesage = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email,))
+        account = cursor.fetchone()
+        if account:
+            mesage = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mesage = 'Invalid email address !'
+        elif not userName or not password or not email:
+            mesage = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password,))
+            mysql.connection.commit()
+            mesage = 'You have successfully registered !'
+    elif request.method == 'POST':
+        mesage = 'Please fill out the form !'
+    return render_template('register.html', mesage=mesage)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
